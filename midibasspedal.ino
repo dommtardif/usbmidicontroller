84eb05089a50d1fe172cf3dec2ec261bf28b912f
Dom <dom@domtardif.info>
2023-01-04 15:54:23 -0500

    finalized changes to receive program changes from guitarx

--- midibasspedal.ino
+++ midibasspedal.ino
@@ -2,13 +2,14 @@
 #include "MIDIUSB.h"
 #include "MCP_ADC.h"
 
+//Declare ADC
 MCP3008 adc;
 
 //Number of pots connected to ADC
 const int potsQty = 3;
 
 //Pots midi controller number
-const int potsMidiController[potsQty] = {16,17,18};
+const int potsMidiController[potsQty] = { 16, 17, 18 };
 
 //Pots previous values
 int potsPrevVal[potsQty];
@@ -17,10 +18,10 @@ int potsPrevVal[potsQty];
 const int buttonsQty = 4;
 
 //Buttons pin definitions
-const int buttonsPin[buttonsQty] = {5,4,3,2};
+const int buttonsPin[buttonsQty] = { 5, 4, 3, 2 };
 
 //Buttons midi controller numner
-const int buttonsMidiController[buttonsQty] = {10,11,12,13};
+const int buttonsMidiController[buttonsQty] = { 10, 11, 12, 13 };
 
 //ADC chip select pin
 const int ADC_CSPin = 10;
@@ -28,74 +29,68 @@ const int ADC_CSPin = 10;
 //Midi channel to operate on
 const byte midiChannel = 0;
 
-void midiPresetReturn(){
-midiEventPacket_t rx;
-  do {
-    rx = MidiUSB.read();
-    if (rx.header != 0) {
-      if (rx.byte1 == 0xC0) {
-        Serial.print("Preset Changed to:");
-        Serial.println(rx.byte2);    
-      }
-      if (rx.byte1 == 0xB0) {
-        Serial.print("Bank Changed to:");
-        Serial.println(rx.byte3);        
-      }      
-      // Serial.print("Received: ");
-      // Serial.print(rx.header, HEX);
-      // Serial.print("-");
-      // Serial.print(rx.byte1, HEX);
-      // Serial.print("-");
-      // Serial.print(rx.byte2, HEX);
-      // Serial.print("-");
-      // Serial.println(rx.byte3, HEX);
-    }
-  } while (rx.header != 0);  
-}
-
 void setup() {
   Serial.begin(115200);
 
   adc.begin(ADC_CSPin);
 
-  for (int button = 0; button < buttonsQty; button++){
+  //Set all pins to input
+  for (int button = 0; button < buttonsQty; button++) {
     pinMode(buttonsPin[button], INPUT);
   }
-
+  //Gather all pots initial values
   for (int port = 0; port < potsQty; port++) {
     potsPrevVal[port] = mapPot(adc.analogRead(port));
   }
 }
 
+//Map ADC reading to midi value 0-127
 int mapPot(int value) {
   return int(map(value, 0, 1023, 0, 127));
 }
 
+//Send midi control change
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = { (byte)0x0B, (byte)0xB0 | channel, control, value };
   MidiUSB.sendMIDI(event);
   MidiUSB.flush();
-  // Serial.print("Control:");
-  // Serial.print(control);
-  // Serial.print("  value:");
-  // Serial.print(value);
-  // Serial.println(" Sent change");
+}
+
+//Receive guitarX preset change
+void midiPresetReturn() {
+  midiEventPacket_t rx;
+  bool receivedPacket = false;
+  do {
+    rx = MidiUSB.read();
+    switch (rx.header) {
+      case 0xC:
+        Serial.print("Preset Changed to:");
+        Serial.println(rx.byte2);
+        break;
+    }
+  }
+  while (rx.header != 0);
 }
 
 void loop() {
+  //Read buttons input and send to midi accordingly
   for (int button = 0; button < buttonsQty; button++) {
     if (digitalRead(buttonsPin[button]) == HIGH) {
       controlChange(midiChannel, buttonsMidiController[button], 127);
       delay(250);
     }
   }
-  midiPresetReturn();  
-  int tmpRead = 0;
+
+  int tmpRead = 0; //temporary reading of pots to send midi only on change
+
+  //Read all pots values and send to midi accordingly
   for (int port = 0; port < potsQty; port++) {
     tmpRead = mapPot(adc.analogRead(port));
     if (tmpRead != potsPrevVal[port]) {
-      controlChange(midiChannel,potsMidiController[port],tmpRead);      
+      controlChange(midiChannel, potsMidiController[port], tmpRead);
       potsPrevVal[port] = tmpRead;
     }
   }
+  
+  midiPresetReturn();
 }
